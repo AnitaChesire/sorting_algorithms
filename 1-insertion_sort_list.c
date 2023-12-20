43db48f063122a00c2611c36c94589acbda6b87a
AnitaChesire <bikamboanita@gmail.com>
2023-12-20 04:22:59 -0800

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,36 +7,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || (*list)->next == NULL)
+    listint_t *node, *back, *current;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *node = (*list)->next;
-
+    node = (*list)->next;
     while (node)
     {
-        listint_t *current = node;
-
+        current = node;
         while (current->prev && current->prev->n > current->n)
         {
-            /* Swap the nodes */
-            current->prev->next = current->next;
-            if (current->next)
-                current->next->prev = current->prev;
+            back = current->prev;
 
-            current->next = current->prev;
-            current->prev = current->prev->prev;
-
-            if (current->prev)
-                current->prev->next = current;
+            /* Perform the swap */
+            if (back->prev)
+                back->prev->next = current;
             else
                 *list = current;
+            current->prev = back->prev;
+            back->next = current->next;
+            if (current->next)
+                current->next->prev = back;
+            current->next = back;
+            back->prev = current;
 
-            current->next->prev = current;
-
-            /* Move to the next node */
             print_list(*list);
         }
-
         node = node->next;
     }
 }
