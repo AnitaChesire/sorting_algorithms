95b2b94ade8dd28656b2f3790495b9a938425384
AnitaChesire <bikamboanita@gmail.com>
2023-12-20 04:15:08 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array elements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, index;
+    int tmp;
+
+    if (size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (index = 0; index < size - i - 1; index++)
+        {
+            if (array[index] > array[index + 1])
+            {
+                tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+}
+
